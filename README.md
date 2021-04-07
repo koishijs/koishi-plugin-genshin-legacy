2209d0769887d1048cac5bc2bc9052f05a5b2cdd
Dragon-Fish <824399619@qq.com>
2021-04-07 22:03:52 +0800

    chore: split i18n data

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# Genshin for Koishi  `koishi-plugin-genshin`
+# koishi-plugin-genshin
 
-**原神四季**（_koishi-plugin-genshin_ ~~征名启事~~）是一个为[koishi](https://github.com/koishijs/koishi)设计的查询原神国服玩家数据的插件。
+**原神四季**（_Genshin Impact for Koishi_ → ~~征名启事~~）是一个为[koishi](https://github.com/koishijs/koishi)设计的查询原神国服玩家数据的插件。
 
-## 安装
+## 特色功能
+
+喜欢玩《原神》？想和沙雕群友一起凹数据？懒得下载米游社？那么本插件将会是您的不二之选！来给您的群聊安排一只能查询原神数据的 koishi 机械姬吧~
+
+为您的机械姬安装本插件，玩家们无需打开米游社，向 koishi bot 发送指令，一键炫耀您的五星角色、五星武器和深渊数据！
+
+## 安装方法
 
 ```bash
 # Via yarn
@@ -11,11 +17,17 @@ yarn add koishi-plugin-genshin
 npm install koishi-plugin-genshin
 ```
 
-## 需求
+## 使用指南
 
-目前插件需要使用数据库才能正常运行！
+具体用法请使用`help genshin`查看（~~作者只是懒得写文档~~）
 
-## 配置
+## 系统需求
+
+需要最新版 koishi v3，并配置数据库。
+
+另外还需要安装 Chromium 用于生成图片。
+
+## 配置项目
 
 插件目前需要使用您的网页版米游社的 cookie 来获取玩家信息。
 
@@ -44,9 +56,13 @@ App.plugin(require('koishi-plugin-genshin'), {
 })
 ```
 
-## 指令
+关于自定义输出排版，可以使用 koishi 的 template 语法，由于字符串太多，[《请您读源码.jpg》](./i18n.js)。
 
-具体用法请使用`help genshin`查看（~~作者只是懒得写文档~~）
+## 注意事项
+
+一个米游社账号一天内只能查询 30 个玩家数据，目前还没有考虑负载均衡的设计，请自行处理频率控制！
+
+~~【广告】如果你喜欢本插件，欢迎给作者打钱或者捐赠米游社账号。~~
 
 ---
 
--- index.js
+++ index.js
@@ -55,45 +55,7 @@ Date.prototype.format = function(fmt) {
 const apply = (koishi, options) => {
   genshin.loginWithCookie(options.cookie)
 
-  template.set('genshin', {
-    // command descriptions
-    cmd_genshin_desc: '获取《原神》玩家信息',
-    cmd_abyss_desc: '查询原神深境螺旋数据。',
-    cmd_character_desc: '查询指定名称的角色的等级与装备信息。',
-    cmd_specify_uid: '查询指定 uid 的信息',
-    // genshin
-    info_regestered: '您的《原神》uid已注册为：{0}',
-    invalid_cn_uid: '您输入的不是合法的《原神》国服uid~',
-    not_registered:
-      '您还没有注册您的《原神》用户信息，请艾特我输入“genshin 游戏内uid”进行注册~（注意打空格）',
-    successfully_registered: '您的《原神》信息注册成功~',
-    // abyss
-    abyss_cur_not_active: '玩家 {0} 还没有开启本期深境螺旋。',
-    abyss_cur_is_active: '玩家 {0} 的本期深境螺旋数据：',
-    abyss_prev_not_active: '玩家 {0} 没有参与上一期深境螺旋。',
-    abyss_prev_is_active: '玩家 {0} 的上一期深境螺旋数据：',
-    abyss_basic_data:
-      '〓基本信息〓\n到达层数：{{ max_floor }}\n战斗次数：{{ total_win_times }}次通关/{{ total_battle_times }}总尝试\n获得渊星：{{ total_star }}',
-    abyss_top_stats:
-      '〓最佳战绩〓\n最强一击：{{ damage_rank }}\n最高承伤：{{ take_damage_rank }}\n最常出场：{{ reveal_rank }}\n元素爆发：{{ energy_skill_rank }}',
-    abyss_cur_time: '本期深境螺旋将于【{0}】结束，还剩 {1}。',
-    abyss_prev_time: '上期深境螺旋从 {0} 开始，于 {1} 结束。',
-    // characters
-    has_character: '玩家 {0} 的 {1}：',
-    no_character: '玩家 {0} 似乎没有名为 {1} 的角色。',
-    character_basic:
-      '{{ avatar }}\n{{ rarity }}★ {{ constellation }}命 {{ name }}\n等级：{{ level }}级，好感：{{ fetter }}级', // {{ portrait }} -> 立绘
-    character_weapon:
-      '〓武器〓\n{{ name }} ({{ rarity }}★{{ type_name }})\n{{ level }}级 ({{ affix_level }}精炼)',
-    character_reliquaries: '〓圣遗物〓\n{0}',
-    has_x_star_characters: '玩家 {0} 一共拥有 {1} 个 {2}★ 角色。',
-    no_x_star_character: '玩家 {0} 似乎没有 {1}★ 角色。',
-    // error messages
-    api_request_failed: '请求数据时出现问题（可能原因：米游社验证信息过期）',
-    failed: '出现了亿点问题：{0}',
-    fetch_data_failed:
-      '出现了亿点问题……（可能原因：玩家uid注册错误或玩家未公开米游社资料。）',
-  })
+  template.set('genshin', require('./i18n'))
 
   // 注册
   koishi
@@ -119,10 +81,14 @@ const apply = (koishi, options) => {
     })
 
   koishi
-    .command('genshin.basic', '警告：实验功能！查询玩家基本信息（宝箱、成就、声望）', {
-      maxUsage: 20,
-      minInterval: Time.minute,
-    })
+    .command(
+      'genshin.basic',
+      '警告：实验功能！查询玩家基本信息（宝箱、成就、声望）',
+      {
+        maxUsage: 20,
+        minInterval: Time.minute,
+      }
+    )
     .userFields(['genshin_uid'])
     .option('uid', `-u <uid:posint> ${template('genshin.cmd_specify_uid')}`)
     .action(async ({ session, opttions }) => {
@@ -190,8 +156,6 @@ const apply = (koishi, options) => {
         const basicFilter = new util.CharactersFilter(userInfo.avatars)
 
         if (!chara) return template('genshin.no_character', uid, name)
-
-        const avatar = basicFilter.name(name).image
 
         function reliquariesFmt(reliquaries) {
           if (reliquaries.length < 1) return '无'
