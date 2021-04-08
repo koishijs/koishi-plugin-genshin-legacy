d9e16a1179bcc7d1b702dfd4eb69d57940d5f173
Dragon-Fish <824399619@qq.com>
2021-04-09 03:21:14 +0800

    update(profile): change render jQuery -> pug

--- module/test.js
+++ module/test.js
@@ -1,5 +1,21 @@
-const fs = require('fs-extra')
+const pug = require('pug')
 const path = require('path')
-require('./profile')().then(file => {
-  fs.writeFileSync(path.resolve(__dirname, 'secret.test.png'), file)
+
+const html = pug.renderFile(path.resolve(__dirname, '../public/profile.pug'), {
+  pretty: 1,
+  uid: 100000001,
+  basic_stats: [
+    { count: 123, desc: '神瞳' },
+    { count: 123, desc: '神瞳' },
+  ],
+  character_list: [
+    {
+      name: 'name',
+      icon: 'http://icon.com',
+      level: 50,
+      fetter: 8,
+      rarity: 4,
+    },
+  ],
 })
+console.log(html)
