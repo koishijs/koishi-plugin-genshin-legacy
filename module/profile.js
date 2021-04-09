2e2047fb972c07bdfb310827216856bec1056dfe
Dragon-Fish <824399619@qq.com>
2021-04-10 00:53:46 +0800

    改进profile

--- module/profile.js
+++ module/profile.js
@@ -1,31 +1,23 @@
 const ppt = require('puppeteer')
 const path = require('path')
-const { segment } = require('koishi-utils')
+const { segment, template } = require('koishi-utils')
 
 module.exports = async ({ uid, userInfo }) => {
   let screenshot
 
-  const browser = await ppt.launch({
-    args: [
-      '--no-sandbox',
-      '--disable-infobars ', // don't show information bar
-      '--lang=zh-CN',
-      '--disable-dev-shm-usage',
-    ],
-    defaultViewport: { width: 800, height: 600 },
-    headless: 1,
-  })
-  const page = await browser.newPage()
-  await page.goto('file:///' + path.resolve(__dirname, '../public/index.html'))
-
   try {
-    await page.evaluate(
-      (userInfo, uid) => {
-        return Profile({ uid, userInfo })
-      },
-      userInfo,
-      uid
-    )
+    const browser = await ppt.launch({
+      args: [
+        '--no-sandbox',
+        '--disable-infobars ', // don't show information bar
+        '--lang=zh-CN',
+        '--disable-dev-shm-usage',
+      ],
+      defaultViewport: { width: 800, height: 600 },
+      headless: 1,
+    })
+    const page = await browser.newPage()
+    await page.setContent(html)
     screenshot = await page.screenshot({
       fullPage: true,
     })
--- module/profile.test.js
+++ module/profile.test.js
@@ -4,17 +4,33 @@ const { writeFileSync } = require('fs')
 
 const { uid, cookie } = require('./secret')
 const { GenshinKit } = require('genshin-kit')
+const { template } = require('koishi-utils')
+
+template.set('genshin', require('../i18n'))
+
+function m(k) {
+  return template(`genshin.profile.${k}`)
+}
 
 const genshin = new GenshinKit()
 genshin.loginWithCookie(cookie)
 genshin.getUserInfo(uid).then((data) => {
+  let _stats = Object.assign({}, data.stats)
+  data.stats = []
+  for (key in _stats) {
+    data.stats.push({ desc: m('stats.' + key), count: _stats[key] })
+  }
+  const options = {
+    ui: {
+      title: m('ui.title'),
+    },
+    uid,
+    ...data,
+  }
+  console.log(options)
   const html = pug.renderFile(
     path.resolve(__dirname, '../public/profile.pug'),
-    {
-      pretty: 1,
-      uid: 100000001,
-      ...data,
-    }
+    options
   )
   writeFileSync(path.resolve(__dirname, '../public/profile.test.html'), html)
 })
