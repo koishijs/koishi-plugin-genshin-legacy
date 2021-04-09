71f85ffb3c0f4ae1da342ef748fb4fcdb4d66bc4
机智的小鱼君 <44761872+Dragon-Fish@users.noreply.github.com>
2021-04-10 03:29:30 +0800

    update: change html render to pug (#4)
    * update: using pug insteed jQuery
    * minor fix

--- module/profile.js
+++ module/profile.js
@@ -1,31 +1,44 @@
 const ppt = require('puppeteer')
+const { segment, template } = require('koishi-utils')
 const path = require('path')
-const { segment } = require('koishi-utils')
+
+function m(k) {
+  return template(`genshin.profile.${k}`)
+}
 
 module.exports = async ({ uid, userInfo }) => {
   let screenshot
 
-  const browser = await ppt.launch({
-    args: [
-      '--no-sandbox',
-      '--disable-infobars ', // don't show information bar
-      '--lang=zh-CN',
-      '--disable-dev-shm-usage',
-    ],
-    defaultViewport: { width: 800, height: 600 },
-    headless: 1,
-  })
-  const page = await browser.newPage()
-  await page.goto('file:///' + path.resolve(__dirname, '../public/index.html'))
+  let _stats = Object.assign({}, userInfo.stats)
+  userInfo.stats = []
+  for (key in _stats) {
+    userInfo.stats.push({ desc: m('stats.' + key), count: _stats[key] })
+  }
+  const options = {
+    ui: {
+      title: m('ui.title'),
+    },
+    uid,
+    ...userInfo,
+  }
+  const html = pug.renderFile(
+    path.resolve(__dirname, '../public/profile.pug'),
+    options
+  )
 
   try {
-    await page.evaluate(
-      (userInfo, uid) => {
-        return Profile({ uid, userInfo })
-      },
-      userInfo,
-      uid
-    )
+    const browser = await ppt.launch({
+      args: [
+        '--no-sandbox',
+        '--disable-infobars ', // don't show information bar
+        '--lang=zh-CN',
+        '--disable-dev-shm-usage',
+      ],
+      defaultViewport: { width: 800, height: 600 },
+      headless: 1,
+    })
+    const page = await browser.newPage()
+    await page.setContent(html)
     screenshot = await page.screenshot({
       fullPage: true,
     })
--- module/test.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs-extra')
-const path = require('path')
-require('./profile')().then(file => {
-  fs.writeFileSync(path.resolve(__dirname, 'secret.test.png'), file)
-})
