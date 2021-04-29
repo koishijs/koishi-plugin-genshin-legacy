61bfd759e38b795214a134a6e83669ab64b07630
Dragon-Fish <824399619@qq.com>
2021-04-29 22:58:59 +0800

    暂存 抽卡模拟

--- module/plugin-wish.js
+++ module/plugin-wish.js
@@ -4,13 +4,13 @@ const { Context, Command } = require('koishi-core')
 const gachaNameToType = {
   角色: 301,
   武器: 302,
-  常驻: 200
+  常驻: 200,
 }
 const gachaAppTypeToName = {
   character: '角色',
   weapon: '武器',
   permanent: '常驻',
-  novice: '新手'
+  novice: '新手',
 }
 
 /**
@@ -21,10 +21,11 @@ const gachaAppTypeToName = {
 function apply(koishi, pOptions) {
   koishi
     .command('genshin.wish')
-    .option('number', '-n <number:posint>')
+    .alias('原神抽卡')
+    .option('number', '-n <number:posint>', { fallback: 1 })
     // .option('name','-n <pool> 选定抽卡时的卡池名称')
     .option('type', '-t <type> 选定抽卡时的卡池类别(角色/武器/常驻)', {
-      fallback: '角色'
+      fallback: '角色',
     })
     .option('list-all', '-L 查看全部可用卡池')
     .option('list', '-l <type> 查看相关类型卡池的内容')
@@ -38,55 +39,127 @@ function apply(koishi, pOptions) {
   async function check({ session, options }) {
     koishi.logger('genshin').info('wish', options)
 
-    session.user.genshin_gacha = session.user.genshin_gacha || {
-      counter: {},
-      result: {}
-    }
-
     if (options['list-all']) {
       const index = await util.getGachaIndex()
-      const poolIds = index.map((i) => i.gacha_id)
+      const poolIds = index.map(i => i.gacha_id)
 
       const queue = []
-      poolIds.forEach((id) => queue.push(util.getGachaData(id)))
-      const pools = await Promise.all(queue)
-      pools.map((i) => util.poolStructureConverter(i))
+      poolIds.forEach(id => queue.push(util.getGachaData(id)))
+      let pools = await Promise.all(queue)
+      pools = pools.map(i => util.poolStructureConverter(i))
 
       return [
         `当前可用的卡池共有 ${pools.length} 个：`,
         pools
-          .map((i) => {
-            return `${i.name} (${i.upSSR.join('、')} UP)`
+          .map(i => {
+            return `${i.name} (${i.upSSR.map(i => i.name).join('、')} UP)`
           })
-          .join('\n')
+          .join('\n'),
       ].join('\n')
     }
 
-    if (options.list && gachaNameToType[options.list]) {
+    if (options.list) {
+      if (!gachaNameToType[options.list])
+        return '没有这个类型的卡池：' + options.list
       let pool = await util.getOfficialGachaPool(gachaNameToType[options.list])
       pool = util.poolStructureConverter(pool)
       return [
         `${pool.name}`,
         `${gachaAppTypeToName[pool.type]}池`,
-        `5星 UP：${pool.upSSR.join('、')}`,
-        `4星 UP：${pool.upSR.join('、')}`
+        `5星 UP：${pool.upSSR.map(i => i.name).join('、') || '无'}`,
+        `4星 UP：${pool.upSR.map(i => i.name).join('、') || '无'}`,
       ].join('\n')
     }
   }
 
-  function action({ session, options }) {
+  async function action({ session, options }) {
     const gacha = new GenshinGachaKit()
-    const { counter, result } = session.user.genshin_gacha
+    let genshin_gacha = session.user.genshin_gacha
+    console.log('1111', genshin_gacha)
+    const { counter, result } = genshin_gacha
+    console.log('2222', { counter, result })
 
-    gacha.setCounter(counter)
-    gacha.setResult(result)
+    if (counter?.total !== undefined && result?.r !== undefined) {
+      gacha.setCounter(counter)
+      gacha.setResult(result)
+    }
+
+    let number = options.number
+    number = Math.min(90, number)
+    number = Math.max(1, number)
+
+    let pool = await util.getOfficialGachaPool(gachaNameToType[options.type])
+    if (!pool) return `无法拉取 ${options.type} 的卡池信息。`
+    pool = util.poolStructureConverter(pool)
+
     gacha.setGachaPool(pool)
 
-    return '在整了在整了……'
+    const items = gacha.multiWish(number)
+
+    const savedData = {
+      counter: gacha.getCounter(),
+      result: gacha.getResult(),
+    }
+    console.log('保存数据', savedData)
+    genshin_gacha = savedData
+    await session.user._update()
+    console.log('数据库中的数据', session.user.genshin_gacha)
+
+    return `抽到了 ${items
+      .map(i => `${i.name}(${i.rarity}星)x${i.count}`)
+      .join('、')}`
   }
+
+  koishi
+    .command('genshin.backpack')
+    .option('reset', '-R')
+    .userFields(['genshin_gacha'])
+    .action(async ({ session, options }) => {
+      let genshin_gacha = session.user.genshin_gacha
+      if (!genshin_gacha) return '您还未抽过卡。'
+
+      if (options.reset) {
+        const gacha = new GenshinGachaKit()
+        gacha.clearCounter().clearResult()
+        genshin_gacha = {
+          counter: gacha.getCounter(),
+          result: gacha.getResult(),
+        }
+        await session.user._update()
+        return '已重置背包。'
+      }
+
+      const { counter, result } = Object.assign({}, genshin_gacha)
+      console.log(JSON.stringify({ counter, result }))
+      return [
+        `您共抽了 ${counter.total} 次卡`,
+        `五星角色：${result.ssr
+          .filter(i => i.type === 'character')
+          .map(i => i.name)
+          .join('、') || '无'}`,
+        `五星武器：${result.ssr
+          .filter(i => i.type === 'weapon')
+          .map(i => i.name)
+          .join('、') || '无'}`,
+        `距离上一次五星 ${counter.lastSSR} 抽，您现在${
+          counter.ensureSSR === 1 ? '有' : '没有'
+        }大保底。`,
+      ].join('\n')
+    })
+
+  koishi
+    .command('arr')
+    .userFields(['arr_test'])
+    .action(async ({ session }) => {
+      const arr = session.user.arr_test || []
+      arr.push({ length: { foo: { bar: [arr.length] } } })
+      session.user.arr_test = arr
+      await session.user._update()
+      return JSON.stringify(arr)
+    })
 }
 
 module.exports = {
   name: 'genshin-wish',
-  apply
+  apply,
 }
